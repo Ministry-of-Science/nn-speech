923848c87b626914904e98350785f0c1bbbdd1f1
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-24 14:56:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* jshint undef: true, unused: true, esversion: 6, node: true */
+
+"use strict";
+
+
+
+/*
+  Simple chart to choose how you want your Navi to sound (a couple presets are available)
+    - high vs deep voice
+    - fast vs slow speaker
+    - curt vs drawn out (Matthew McConnahey in his Lincoln)
+
+  - speech cadence
+*/
